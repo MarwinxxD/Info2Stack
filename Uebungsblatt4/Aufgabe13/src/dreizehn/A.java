72e35a872742772dfc99e319f2889a105c068ff1
Marwin <merklmarwin@gmail.com>
2024-05-24 14:57:31 +0200

    finished Aufgabe13 auf Übungsblatt 4

---
+++ Uebungsblatt4/Aufgabe13/src/dreizehn/A.java
@@ -0,0 +1,36 @@
+package dreizehn;
+
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class A {
+    static Collection<Integer> firstCollection = new LinkedList<>();
+
+    static Collection<Object> secondCollection = new HashSet<>();
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            int rand = new Random().nextInt(10);
+
+            firstCollection.add(rand);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            int rand = new Random().nextInt(10);
+
+            secondCollection.add(rand);
+        }
+
+        if (secondCollection.containsAll(firstCollection)) {
+            System.out.println("secondCollection enthaelt alle Elemente von firstCollection");
+        } else {
+            System.out.println("secondCollection enthaelt nicht alle Elemente von firstCollection");
+        }
+    }
+
+
+}
---
+++ Uebungsblatt4/Aufgabe13/src/dreizehn/B.java
@@ -0,0 +1,44 @@
+package dreizehn;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
+
+public class B {
+
+    public static void main(String[] args) {
+        String a = "Polymorphismus";
+        StringBuilder b = new StringBuilder("spätes Binden");
+        LocalDate c = LocalDate.now();
+        Random d = new Random();
+        Scanner e = new Scanner(System.in);
+        ArrayList<Object> poly = new ArrayList<>();
+
+        poly.add(a);
+        poly.add(b);
+        poly.add(c);
+        poly.add(d);
+        poly.add(e);
+
+        Iterator<Object> it = poly.iterator();
+
+        while(it.hasNext()) {
+            Object o = it.next();
+            if(o.getClass() == LocalDate.class) {
+                System.out.println(((LocalDate) o).minusWeeks(2));
+            } else if (o instanceof CharSequence) {
+                 o = (CharSequence) o;
+                 System.out.println(((CharSequence) o).charAt(0) + "" +  ((CharSequence) o).charAt(((CharSequence) o).length() - 1));
+            } else {
+                System.out.println(o);
+            }
+        }
+    }
+
+    //Scanner und Random unterscheiden sich darin, dass Random nur java.util.Random@ und ein Nummer printed,
+    //während Scanner noch eine einige Eingenschaften, wie eine aktuelle Position, einen delimited, usw. printed
+
+
+}
