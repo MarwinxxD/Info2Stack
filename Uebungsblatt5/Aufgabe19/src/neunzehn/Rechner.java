44104bcba03db3f704766acc4979c8b68b7ee757
MarwinxxD <merklmarwin@gmail.com>
2024-06-05 20:47:37 +0200

    finished Aufgabe 19 Blatt 5

--- Uebungsblatt5/Aufgabe19/src/neunzehn/Rechner.java
+++ Uebungsblatt5/Aufgabe19/src/neunzehn/Rechner.java
@@ -1,5 +1,7 @@
 package neunzehn;
 
+import java.util.Arrays;
+
 public class Rechner {
 
     public void rechnen(String str) throws NumberFormatException, IllegalArgumentException{
@@ -7,31 +9,35 @@ public class Rechner {
 
         String[] erg = str.split("[\\x2B\\x2D\\x2A\\x2F]");
 
-        if (erg.length > 3) {
+        if (erg.length > 2) {
             throw new IllegalArgumentException("Zu viele Inputs");
-        } else if (erg.length < 3) {
+        } else if (erg.length < 2) {
             throw new IllegalArgumentException("Zu wenige Inputs");
         }
 
+        erg[0] = erg[0].replaceAll("\\s+", "");
+        erg[1] = erg[1].replaceAll("\\s+", "");
+
         if(erg[0].matches("[\\x2B\\x2D\\x2A\\x2F]")
-                || erg[2].matches("[\\x2B\\x2D\\x2A\\x2F]")) {
+                || erg[1].matches("[\\x2B\\x2D\\x2A\\x2F]")) {
             throw new IllegalArgumentException("Zu viele Operatoren");
         }
 
         try {
             x = Integer.parseInt(erg[0]);
-            y = Integer.parseInt(erg[2]);
+            y = Integer.parseInt(erg[1]);
         } catch (NumberFormatException e) {
             throw new NumberFormatException("Die erste und/oder zweite Zahl ist illegal");
         }
 
-        if(erg[1].charAt(0) == '+') {
-            System.out.println("= " + x + y);
-        } else if (erg[1].charAt(0) == '-') {
-            System.out.println("= " +  - y);
-        } else if (erg[1].charAt(0) == '*') {
-            System.out.println("= " + x * y);
+        if(str.contains("+")) {
+            System.out.println("= " + (x + y));
+        } else if (str.contains("-")) {
+            System.out.println("= " + (x - y));
+        } else if (str.contains("*")) {
+            System.out.println("= " + (x * y));
+        } else {
+            System.out.println("= " + (x / y));
         }
-        System.out.println("= " + x / y);
     }
 }
--- Uebungsblatt5/Aufgabe19/src/neunzehn/RechnerMain.java
+++ Uebungsblatt5/Aufgabe19/src/neunzehn/RechnerMain.java
@@ -6,7 +6,9 @@ public class RechnerMain {
     public static void main(String[] args) throws IllegalArgumentException{
         System.out.println("Dieses Programm ist ein Rechner, welcher eine Kette von Zeichen annimmt:");
         System.out.println("Diese Zeichen müssen erste eine Zahl, dann einen Operator und dann noch eine Zahl beinhalten");
-        System.out.println("annimmt und das Ergebnis ausgibt.");
+        System.out.println("Der Rechner rechnet darauffolgend das Ergebnis aus.");
+        System.out.println("Mit dem Input EXIT bricht der Rechner ab.");
+
 
         System.out.println("\nBitte gib deine gewünschte Rechnung ein!");
 
